98fd3a824f52bd8e35b7ccbcda6e622c89be9313
enenkao1 <enen_kao@126.com>
2023-10-24 03:55:47 +1100

    post Renew

--- src/views/postView.vue
+++ src/views/postView.vue
@@ -1,49 +1,77 @@
 <template >
-<div style="height: 100vh;background-color: gray">
+<div style="height: 150vh;background-color: gray">
   <div>
-    <el-container style="margin: auto;height: 15vh;background-color: white;justify-content: center;border-bottom: 10px solid black">
+    <el-container class="container">
       <img src="@/assets/logo.png" alt="" style = "width: 60%">
     </el-container>
   </div>
-  <div style="width: 90%;margin: auto;height: 80vh;background-color: white;">
-    <div style="height: 5vh;display: flex;align-items: center">
-      <div size="small" style="flex: 1">
-        <el-button size="small" style="float: left;margin-left: 30px" @click = "goBack">back</el-button>
+  <div style="width: 90%;margin: auto;height: 130vh;background-color: white;display: flex; flex-direction: column;">
+    <div style="flex: 1">
+      <div style="height: 5vh;display: flex;align-items: center">
+        <div size="small" style="flex: 1">
+          <el-button size="small" style="float: left;margin-left: 30px" @click = "goBack">back</el-button>
+        </div>
+        <el-pagination
+            style="float: right;"
+            small
+            layout="prev, pager, next"
+            :total="30"
+        >
+        </el-pagination>
       </div>
-      <el-pagination
-          style="float: right;"
-          small
-          layout="prev, pager, next"
-          :total="30"
-      >
-      </el-pagination>
-    </div>
 
       <el-container style="width: 100%;margin: auto;">
         <el-header height="20px" style="background-color: azure;"> </el-header>
         <el-container>
-          <el-aside  width="300px" style="background-color: cadetblue;line-height: 60px">
+          <el-aside  width="250px" class="postAside">
             <img src="@/assets/User.jpg" alt="" style = "width: 50%;border: 2px solid white;margin-top: 20px">
-            <h2 style="text-align: center">Username</h2>
+            <div class="username">Username</div>
           </el-aside>
           <el-main  style="background-color: white;border: 2px solid #000000">
-           <div style="border-bottom: 2px dashed black;height: 2vh;text-align: left">
-            Published: Today
-           </div>
+            <div style="border-bottom: 2px dashed black;height: 2vh;text-align: left">
+              Published: Today
+            </div>
             <div style="text-align: left;margin-top: 20px">
               Content
             </div>
           </el-main>
         </el-container>
       </el-container>
+    </div>
+
+      <el-container class="replyInput">
+        <el-aside  width="250px" class="elAside">
+          <img src="@/assets/User.jpg" alt="Image" style = "width: 50%;border: 2px solid white;margin-top: 20px;">
+          <div style="" class="username">Your Username</div>
+        </el-aside>
+        <el-main  style="background-color: white;border: 2px solid #000000">
+          <div class="replyInput">
+            <el-input
+                type="textarea"
+                :rows="10"
+                placeholder="请输入内容"
+                v-model="textarea"
+                style="border: 3px solid black;margin-bottom: 20px">
+            </el-input>
+            <el-button type="primary" style="float: left">Post</el-button>
+          </div>
+        </el-main>
+      </el-container>
+
 
   </div>
+
 </div>
 </template>
 
 <script>
 export default {
   name: "postView",
+  data() {
+    return {
+      textarea: ''
+    }
+  },
   methods:{
     goBack(){
       this.$router.push("/forum");
@@ -53,5 +81,39 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  margin: auto;
+  height: 15vh;
+  background-color: white;
+  justify-content: center;
+  border-bottom: 10px solid black;
+}
 
+.replyInput{
+  flex:0;
+  width: 100%;
+  align-self: center;
+
+}
+.postAside{
+  background-color: cadetblue;
+  display:flex;
+  flex-direction:column;
+  align-items: center;
+  justify-content: center;
+  height: 240px;
+}
+.elAside{
+  background-color: cadetblue;
+  display:flex;
+  flex-direction:column;
+  align-items: center;
+  justify-content: center;
+}
+.username{
+  margin-top: 15px;
+  padding: 0;
+  font-size: 20px;
+  font-weight: bold;
+}
 </style>
